404c5a303eed1d49fed96a753c2f2e823bfef3f8
dbd2h <kangminchang1@gmail.com>
2022-03-04 00:44:30 +0900

    4th-project

--- 4th-project/index.js
+++ 4th-project/index.js
@@ -6,4 +6,8 @@ $button.addEventListener("mousedown", (event) => {
   const btnY = btnPosition.top;
   const btnWidth = btnPosition.width;
   const btnHeight = btnPosition.height;
+  console.log(btnX, btnY);
 });
+
+//원리: 안에 ripple 클래스를 단 div를 하나 만들어주고 클릭하면 스케일이 버튼의
+// 크기로 해주며, 색깔도 변하게 하는 거
