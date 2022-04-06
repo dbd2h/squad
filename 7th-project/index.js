dc8ce05b420bd3d2b01653b64ba9a437e138e975
dbd2h <kangminchang1@gmail.com>
2022-04-07 02:45:15 +0900

    project-5,6,7

---
+++ 7th-project/index.js
@@ -0,0 +1,8 @@
+const $introVideo = document.querySelector(".intro_background>video");
+const $introLogo1 = document.querySelector(".intro_logo1");
+const $introLogo2 = document.querySelector(".intro_logo2");
+
+$introVideo.play();
+$introLogo1.style.background = `url(
+  "https://wstatic-cdn.universe-official.io/universe/v1/img/logo_1.png"
+) 50% 0`;
